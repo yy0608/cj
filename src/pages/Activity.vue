959b90e1c21455461de6632cb9ffeb12cc2f0cc2
youyi <447462224@qq.com>
2018-05-25 00:01:07 +0800

    活动和领取页面

--- src/pages/Activity.vue
+++ src/pages/Activity.vue
@@ -1,9 +1,153 @@
 <template>
-<div class="page-item">activity.html</div>
+<div class="page-item">
+  <div class="my-slider">
+    <slider ref="slider" :pages="pages" :sliderinit="sliderinit" @slide='slide' @tap='onTap' @init='onInit'>
+      <!-- 设置loading,可自定义 -->
+      <div slot="loading">loading...</div>
+    </slider>
+    <img class="banner-txt" src="../assets/imgs/banner_txt.png">
+  </div>
+  <div class="order-cont">
+    <div class="tc">
+      <img class="cash" src="../assets/imgs/cash.png">
+    </div>
+    <div class="form-cont">
+      <div class="form-item">
+        <div class="label">姓名</div>
+        <input type="text">
+      </div>
+      <div class="form-item">
+        <div class="label">电话</div>
+        <input type="text">
+      </div>
+      <div class="form-item">
+        <div class="label">城市</div>
+        <input type="text">
+      </div>
+      <div class="form-item">
+        <div class="label">楼盘</div>
+        <input type="text">
+      </div>
+      <div class="tc order-btn">点击预约</div>
+      <div class="tc custom-btn">定制须知</div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
-export default {}
+import slider from 'vue-concise-slider'
+
+export default {
+  data () {
+    return {
+      pages: [{
+        // html: '<div class="slider1">slider1</div>',
+        style: {
+          'background': 'url(' + require('../assets/imgs/tmp/1.jpg') + ')'
+        }
+      },
+      {
+        // html: '<div class="slider2">slider2</div>',
+        style: {
+          'background': 'url(' + require('../assets/imgs/tmp/1.jpg') + ')'
+        }
+      },
+      {
+        // html: '<div class="slider3">slider3</div>',
+        style: {
+          'background': 'url(' + require('../assets/imgs/tmp/1.jpg') + ')'
+        }
+      }],
+      sliderinit: {
+        // currentPage: 0,
+        // thresholdDistance: 500,
+        // thresholdTime: 100,
+        autoplay: 5000,
+        loop: true,
+        // direction: 'row',
+        // infinite: 1,
+        // slidesToScroll: 1,
+        // timingFunction: 'ease',
+        duration: 300
+      }
+    }
+  },
+  components: {
+    slider
+  },
+  methods: {
+    slide () {},
+    onTap () {},
+    onInit () {}
+  }
+}
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import '../assets/css/reset.scss';
+@import '../assets/css/common.scss';
+
+.my-slider {
+  position: relative;
+  height: 5rem;
+  .banner-txt {
+    position: absolute;
+    width: 5.22rem;
+    height: 3.1rem;
+    top: 50%;
+    left: 50%;
+    margin-left: -2.61rem;
+    margin-top: -1.55rem;
+    z-index: 10;
+    pointer-events: none;
+  }
+}
+.order-cont {
+  padding: 0 .3rem;
+  background-color: #22504d;
+  .cash {
+    width: 4.9rem;
+    height: .38rem;
+  }
+  .form-cont {
+    margin-top: .2rem;
+    padding: .3rem 1rem;
+    background-color: #fff;
+  }
+  .form-item {
+    display: flex;
+    align-items: center;
+    position: relative;
+    &:after {
+      content: '';
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      background-color: #e0e0e0;
+      width: 100%;
+      height: 1px;
+      transform: scaleY(.5);
+    }
+    .label {
+      flex-shrink: 0;
+    }
+    input {
+      height: 1rem;
+      padding-left: .3rem;
+      flex-grow: 1;
+      border-style: none;
+    }
+  }
+  .order-btn {
+    margin: .3rem 0;
+    width: 4.86rem;
+    height: .96rem;
+    line-height: .96rem;
+    color: #fff;
+    font-size: .32rem;
+    background: url('../assets/imgs/order-btn.png') no-repeat center center;
+    background-size: 100% auto;
+  }
+}
+</style>
--- src/pages/Get.vue
+++ src/pages/Get.vue
@@ -1,9 +1,108 @@
 <template>
-<div class="page-item">get.html</div>
+<div class="page-item get-cont">
+  <div class="tc logo-cont">
+    <img class="logo" src="../assets/imgs/get_logo.png">
+  </div>
+  <div class="get-block">
+    <div class="money-cont">
+      <div class="symbol">&yen;</div>
+      <div class="number">200</div>
+    </div>
+  </div>
+  <div class="get-tips">
+    <div class="title-cont">
+      <div class="title-inner">贴心提示</div>
+    </div>
+    <ul>
+      <li>在创家消费凭本券可抵押现金使用</li>
+      <li>本券可与其他优惠活动重叠使用</li>
+      <li>本券仅限预约实名使用一次</li>
+      <li>本券不可叠加使用</li>
+      <li>最终解释权归创家家居所有</li>
+    </ul>
+  </div>
+</div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import '../assets/css/reset.scss';
+@import '../assets/css/common.scss';
+
+html, body {
+  height: 100%;
+}
+.get-cont {
+  min-height: 100%;
+  padding: .3rem;
+  box-sizing: border-box;
+  background: url('../assets/imgs/get_bg.png') no-repeat center center;
+  background-size: 100% 100%;
+}
+.logo {
+  width: 2.88rem;
+  height: 1.02rem;
+}
+.get-block {
+  margin: .3rem auto;
+  width: 5.98rem;
+  height: 6.58rem;
+  background: url('../assets/imgs/get_block.png') no-repeat center center;
+  background-size: 100% center;
+}
+.money-cont {
+  display: flex;
+  height: 5.4rem;
+  line-height: 5.4rem;
+  align-items: baseline;
+  justify-content: center;
+  color: #ffb90b;
+  font-family: "PingFang SC", "Hiragino Sans GB", "Heiti SC", "Microsoft YaHei", "WenQuanYi Micro Hei";
+  .symbol {
+    font-size: .8rem;
+  }
+  .number {
+    font-size: 1.6rem;
+  }
+}
+.get-tips {
+  color: #fff;
+  .title-cont {
+    display: flex;
+    justify-content: center;
+  }
+  .title-inner {
+    position: relative;
+    padding: 0 .45rem;
+    height: .5rem;
+    line-height: .5rem;
+    &:before {
+      content: '';
+      position: absolute;
+      left: 0;
+      width: 2rem;
+      height: 100%;
+      background: url('../assets/imgs/dots.png') no-repeat left center;
+    }
+    &:after {
+      content: '';
+      position: absolute;
+      right: 0;
+      width: 2rem;
+      height: 100%;
+      background: url('../assets/imgs/dots.png') no-repeat right center;
+    }
+  }
+  ul {
+    padding: 0 1.2rem;
+    margin-top: .2rem;
+    list-style: disc;
+  }
+  li {
+    line-height: .5rem;
+  }
+}
+</style>
