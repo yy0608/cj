6d23b81b596683152637600c70786d498642a7de
youyi <447462224@qq.com>
2018-05-29 23:55:04 +0800

    相对路径后的图片引用问题修复

--- build/utils.js
+++ build/utils.js
@@ -47,6 +47,7 @@ exports.cssLoaders = function (options) {
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
+        publicPath: '../../', // 解决相对路径
         fallback: 'vue-style-loader'
       })
     } else {
