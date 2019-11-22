7b66ee7d93d7aca2b1aa9549e6293b2c8534b2fb
youyi <447462224@qq.com>
2019-11-22 18:05:58 +0800

    开发

--- src/pages/Order.vue
+++ src/pages/Order.vue
@@ -1,59 +1,64 @@
 <template>
-<div class="page-item">
-  <div>我要预约</div>
+<div class="page-item order-page-cont">
+  <img class="logo-img" src="../assets/imgs/order/logo.png">
+  <div class="tc bg-cont">
+    <img src="../assets/imgs/order/backbg.png">
+    <img class="forbg" src="../assets/imgs/order/forbg.png">
+  </div>
+  <div class="steps-cont">
+    <div
+      class="step-item"
+      v-for="(item, index) in stepsList"
+      :key="index">
+      <img :src="require('../assets/imgs/order/' + item.img)" class="img">
+      <div class="tc txt">{{item.text}}</div>
+      <div class="rect">{{index + 1}}</div>
+    </div>
+  </div>
+  <div class="form-cont">
+    <div class="title">
+      <div class="label">现名额仅剩</div>
+      <div class="number">99</div>
+    </div>
+    <div class="form-item">
+      <input type="text" placeholder="输入姓名获得免费设计">
+    </div>
+    <div class="form-item">
+      <input type="text" placeholder="方便专业设计师联系您">
+    </div>
+    <div class="tips">*预约成功即可免费预览同户型3D全景效果图</div>
+    <div class="submit-cont">
+      <img src="../assets/imgs/order/order.png">
+    </div>
+  </div>
 </div>
 </template>
 
 <script>
-import axios from 'axios'
-import { origin, staticOrigin } from '@/config'
-import OrderBtn from '@/components/OrderBtn.vue'
+// import axios from 'axios'
+// import { origin, staticOrigin } from '@/config'
 
 export default {
   data () {
     return {
-      teamList: [],
-      showRrUrl: false,
-      qrUrl: ''
-    }
-  },
-  components: {
-    OrderBtn
-  },
-  created () {
-    this.$indicator.open({ spinnerType: 'fading-circle' })
-    axios({
-      url: origin + '/cjjjapi/wx/findBizBizTeamGroup.action',
-      method: 'post',
-      data: {}
-    })
-      .then(res => {
-        this.$indicator.close()
-        if (res.data.code) {
-          return this.$toast(res.data.message)
+      stepsList: [
+        {
+          img: 'step1.png',
+          text: '团购价验房'
+        },
+        {
+          img: 'step2.png',
+          text: '免费上门量尺'
+        },
+        {
+          img: 'step3.png',
+          text: '免费设计出图'
+        },
+        {
+          img: 'step4.png',
+          text: '免费送货安装'
         }
-        this.teamList = res.data.data
-      })
-      .catch(err => {
-        this.$indicator.close()
-        console.log(err)
-        this.$toast('客户端请求出错')
-      })
-  },
-  methods: {
-    handleShowQrUrl (url) {
-      this.showRrUrl = true
-      this.qrUrl = url
-    },
-    imgError (e) {
-      this.$toast('图片加载失败')
-      e.currentTarget.src = 'https://dn-placeholder.qbox.me/200x200/f5f5f5/888'
-    },
-    isEmptyUrl (url) {
-      if (!url || url === location.protocol + staticOrigin + '/') {
-        return true
-      }
-      return false
+      ]
     }
   }
 }
@@ -63,80 +68,71 @@ export default {
 @import '../assets/css/reset.scss';
 @import '../assets/css/common.scss';
 
-.page-item {
-  padding-bottom: 1rem;
-}
-
-.team-cont-item {
-  padding: .2rem 0 .2rem .2rem;
-  .title {
-    padding-left: .1rem;
-    margin: .2rem 0;
-    font-size: .32rem;
+.order-page-cont {
+  .logo-img {
+    width: 4.4rem;
   }
-  .team-item {
+  .bg-cont {
     position: relative;
+    & > img {
+      max-width: 100%;
+    }
+    .forbg {
+      position: absolute;
+      width: 7.2rem;
+      // height: 3.6rem;
+      left: 50%;
+      top: 1.4rem;
+      margin-left: -3.6rem;
+    }
+  }
+  .steps-cont {
     display: flex;
     align-items: center;
-    padding: .2rem;
+    margin: 0 .3rem;
+    justify-content: space-between;
+  }
+  .step-item {
+    position: relative;
+    .img {
+      width: 1.4rem;
+    }
+    .txt {
+      margin-top: .1rem;
+      font-size: .24rem;
+      color: #1F524E;
+    }
+    .rect {
+      position: absolute;
+      left: -.15rem;
+      top: -.15rem;
+      width: .4rem;
+      height: .4rem;
+      line-height: .4rem;
+      font-size: .26rem;
+      text-align: center;
+      color: #FBE706;
+      background-color: #1F524E;
+    }
     &:after {
       content: '';
       position: absolute;
-      left: 0;
-      bottom: 0;
-      background-color: #e0e0e0;
-      width: 100%;
-      height: 1px;
-      transform: scaleY(.5);
+      right: -.3rem;
+      top: 50%;
+      margin-top: -.2rem;
+      width: .24rem;
+      height: .24rem;
+      background: url('../assets/imgs/order/triangle.png') no-repeat center center;
+      background-size: contain;
     }
-    .avatar {
-      flex-shrink: 0;
-      width: 1.2rem;
-      height: 1.2rem;
-      img {
-        width: 100%;
-        height: 100%;
-        border-radius: 50%;
-      }
-    }
-    .text-cont {
-      flex-grow: 1;
-      margin-left: .2rem;
-    }
-    .title-cont {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      .name {
-        font-size: .3rem;
-      }
-      .wechat {
-        padding: .1rem .2rem;
-        font-size: .24rem;
-        color: #fff;
-        background-color: #245350;
-      }
-    }
-    .intro {
-      margin-top: .15rem;
-      color: #666;
-      line-height: .36rem;
+    &:last-child:after {
+      display: none;
     }
   }
-}
-.qr-cont {
-  position: fixed;
-  display: flex;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, .75);
-  align-items: center;
-  justify-content: center;
-  & > img {
-    max-width: 80%;
-    max-height: 80%;
+  .form-cont {
+    margin: .3rem;
+    border: 1px solid #00474D;
+    border-radius: .1rem;
   }
 }
 </style>
