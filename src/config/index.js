5534abe9a83ef2060c5fd31d2f3969b2a5df8b7f
youyi <447462224@qq.com>
2019-11-28 10:02:56 +0800

    接口配置修改

--- src/config/index.js
+++ src/config/index.js
@@ -5,21 +5,21 @@ let originOrder = ''
 let staticOrigin = ''
 
 if (process.env.NODE_ENV === 'development') { // 开发环境
-  origin = 'http://14.215.51.234:10003'
+  origin = 'http://47.106.98.124'
   originOrder = 'http://14.215.51.234:10003'
   // staticOrigin = origin + '/cjjjapi'
-  staticOrigin = '//www.cjhome.vip/cjjjapi'
+  staticOrigin = 'http://www.cjhome.vip/cjjjapi'
 } else { // 编译环境
   // 测试环境
   if (process.env.type === 'test') {
-    origin = 'http://14.215.51.234:10003'
+    origin = 'http://47.106.98.124'
     originOrder = 'http://14.215.51.234:10003'
   // 正式环境
   } else {
-    origin = '//47.106.98.124'
+    origin = 'http://47.106.98.124'
     originOrder = 'http://www.cjhome.vip' // 预约项目单独的
   }
-  staticOrigin = '//www.cjhome.vip/cjjjapi'
+  staticOrigin = 'http://www.cjhome.vip/cjjjapi'
 }
 
 module.exports = {
